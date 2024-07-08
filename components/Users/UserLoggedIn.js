94d12a475b8c21f505ff38c52d2ca999da6d93d1
jesjin <jess.hk.jin@gmail.com>
2024-07-08 22:17:41 +0100

    Login UI tweaks

--- components/Users/UserLoggedIn.js
+++ components/Users/UserLoggedIn.js
@@ -1,7 +1,12 @@
-import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
-import { useContext } from "react";
-import { GlobalContext } from "../../context/global-context";
-import { GlobalStyles } from "../../constants/styles";
+import React, { useContext } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native';
+import { GlobalContext } from '../../context/global-context';
 
 const UserLoggedIn = ({ gotoLoggingIn }) => {
   const { user, setUser, setToken, setIsLoggedIn } = useContext(GlobalContext);
@@ -9,46 +14,62 @@ const UserLoggedIn = ({ gotoLoggingIn }) => {
   const handleLogOut = () => {
     setIsLoggedIn(false);
     setUser({});
-    setToken("");
+    setToken('');
     gotoLoggingIn();
   };
 
   return (
-    <View style={styles.rootContainer}>
-      <View style={styles.inputContainer}>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.rootContainer}>
         <Text style={styles.label}>
           Logged in as: {user.first_name} {user.last_name}
         </Text>
-        <View style={styles.buttonWrapper}>
-          <Button title="Log Out" onPress={handleLogOut} />
-        </View>
+        <TouchableOpacity style={styles.button} onPress={handleLogOut}>
+          <Text style={styles.buttonText}>Log Out</Text>
+        </TouchableOpacity>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
 export default UserLoggedIn;
 
 const styles = StyleSheet.create({
-  rootContainer: {
+  safeArea: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
   },
-  inputContainer: {
-    marginHorizontal: 4,
-    marginVertical: 8,
-    width: "80%",
+  rootContainer: {
+    width: '100%',
+    maxWidth: 400,
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    alignItems: 'center',
   },
   label: {
     fontSize: 18,
-    color: GlobalStyles.colors.primary100,
-    color: "black",
-    marginBottom: 4,
-    textAlign: "center",
+    color: 'black',
+    marginBottom: 16,
+    textAlign: 'center',
   },
-  buttonWrapper: {
-    marginVertical: 8,
+  button: {
+    backgroundColor: 'darkslateblue',
+    padding: 10,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
